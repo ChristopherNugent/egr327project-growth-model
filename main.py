c1a07d4aca637b4d2297d7e51107358ae9e84520
Christopher Nugent <awedeven+github@gmail.com>
2017-11-03 15:25:25 -0700

    Commit existing code

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+
+"""
+Models the file growth rate of my egr327 with cyclic indexing. Since the project
+expects a flat file system, this modification is made to prevent extreme file growth,
+as well as ensure constant interaction with other scheduled activities.
+"""
+
+step, size = 1, 0
+entries = []
+last = 0
+try:
+    # Continue iterating until forcibly canceled via ctrl-C
+    while True:
+        entries.append(step % 100)
+        try:
+            entries.remove(random.randint(0, 99))
+        except ValueError:
+            # Doesn't matter if value not found
+            pass
+        if step % 10000 == 0:
+            print('Size: {0:,} | Step; {1:,} | % of original {2:.3f} | Growth: '
+                .format(len(entries), step, 100 * len(entries)/step), len(entries) - last)
+            last = len(entries)
+        step += 1
+except KeyboardInterrupt:
+    print('\nSize of {:,} entries after {:,} steps, which is {:.4f}% of the expected number'
+        .format(len(entries), step, 100 * len(entries)/(step - 100)))
